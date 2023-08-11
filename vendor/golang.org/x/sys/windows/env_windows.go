321ef9d38684c6cef5e9a26f0fac540422edd4d7
Keith Avery <slabgorb@gmail.com>
2023-08-11 13:46:56 -0400

    chore(lib): go mod vendor again sorry

--- vendor/golang.org/x/sys/windows/env_windows.go
+++ vendor/golang.org/x/sys/windows/env_windows.go
@@ -8,6 +8,7 @@ package windows
 
 import (
 	"syscall"
+	"unicode/utf16"
 	"unsafe"
 )
 
@@ -37,14 +38,20 @@ func (token Token) Environ(inheritExisting bool) (env []string, err error) {
 		return nil, err
 	}
 	defer DestroyEnvironmentBlock(block)
-	blockp := unsafe.Pointer(block)
+	blockp := uintptr(unsafe.Pointer(block))
 	for {
-		entry := UTF16PtrToString((*uint16)(blockp))
+		entry := (*[(1 << 30) - 1]uint16)(unsafe.Pointer(blockp))[:]
+		for i, v := range entry {
+			if v == 0 {
+				entry = entry[:i]
+				break
+			}
+		}
 		if len(entry) == 0 {
 			break
 		}
-		env = append(env, entry)
-		blockp = unsafe.Add(blockp, 2*(len(entry)+1))
+		env = append(env, string(utf16.Decode(entry)))
+		blockp += 2 * (uintptr(len(entry)) + 1)
 	}
 	return env, nil
 }
