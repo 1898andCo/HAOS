2eaf7f23c17c8281e3f839fdc1e7a44b9b091d31
Keith Avery <slabgorb@gmail.com>
2023-09-08 13:55:47 -0400

    chore(libs): update docker/docker to 17
    this affected the docker/mount package, which does nto exist in this
    version of the package, so we needed
    to switch to moby/sys/mount

--- pkg/enterchroot/ensureloop.go
+++ pkg/enterchroot/ensureloop.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"os/exec"
 
-	"github.com/1898andCo/HAOS/pkg/system"
-	"github.com/docker/docker/pkg/mount"
+	"github.com/moby/sys/mount"
+	"github.com/moby/sys/mountinfo"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
@@ -15,7 +15,7 @@ import (
 )
 
 func mountProc() error {
-	if ok, err := mount.Mounted("/proc"); ok && err == nil {
+	if ok, err := mountinfo.Mounted("/proc"); ok && err == nil {
 		return nil
 	}
 	logrus.Debug("mkdir /proc")
@@ -23,7 +23,7 @@ func mountProc() error {
 		return err
 	}
 	logrus.Debug("mount /proc")
-	return mount.ForceMount("proc", "/proc", "proc", "")
+	return mount.Mount("proc", "/proc", "proc", "")
 }
 
 func mountDev() error {
@@ -35,7 +35,7 @@ func mountDev() error {
 		return err
 	}
 	logrus.Debug("mounting /dev")
-	return mount.ForceMount("none", "/dev", "devtmpfs", "")
+	return mount.Mount("none", "/dev", "devtmpfs", "")
 }
 
 func mknod(path string, mode uint32, major, minor int) error {
--- pkg/enterchroot/enter.go
+++ pkg/enterchroot/enter.go
@@ -11,9 +11,9 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/1898andCo/HAOS/pkg/system"
-	"github.com/docker/docker/pkg/mount"
 	"github.com/docker/docker/pkg/reexec"
+	"github.com/moby/sys/mount"
+	"github.com/moby/sys/mountinfo"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
@@ -207,7 +207,7 @@ func inFile() (string, uint64, error) {
 }
 
 func run(data string) error {
-	mounted, err := mount.Mounted(data)
+	mounted, err := mountinfo.Mounted(data)
 	if err != nil {
 		return errors.Wrapf(err, "checking %s mounted", data)
 	}
@@ -270,7 +270,7 @@ func run(data string) error {
 		return errors.Wrap(err, "pivot_root failed")
 	}
 
-	if err := mount.ForceMount("", ".", "none", "rprivate"); err != nil {
+	if err := mount.Mount("", ".", "none", "rprivate"); err != nil {
 		return errors.Wrapf(err, "making . private %s", data)
 	}
 
