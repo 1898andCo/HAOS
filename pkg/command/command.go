c3fda3f562aebb64777c24a13ad4c49ae9c0a41d
Keith Avery <slabgorb@gmail.com>
2023-12-12 11:42:50 -0500

    tests: add tests to command pkg

--- pkg/command/command.go
+++ pkg/command/command.go
@@ -7,20 +7,42 @@ import (
 	"os/exec"
 	"strings"
 
+	"context"
+
 	"github.com/sirupsen/logrus"
 )
 
-func ExecuteCommand(commands []string) error {
+// Wrap the exec call in a struct so that we can mock it in our tests.
+type Shell interface {
+	// Execute runs the given command and returns the output and error.
+	Execute(ctx context.Context, cmd string) (output []byte, err error)
+}
+
+// LocalShell is the default shell that executes commands locally, basically
+// calling `sh -c <cmd>` at the end of the day.
+type LocalShell struct{}
+
+func (LocalShell) Execute(ctx context.Context, cmd string) ([]byte, error) {
+	wrapperCmd := exec.CommandContext(ctx, "sh", "-c", cmd)
+	return wrapperCmd.CombinedOutput()
+}
+
+// DefaultShell will be overridden in tests
+// Set the default shell to be the local shell.
+// TODO: still not happy with this pattern
+var DefaultShell Shell = LocalShell{}
+
+func ExecuteCommand(commands []string) ([]string, error) {
+	var results []string
 	for _, cmd := range commands {
 		logrus.Debugf("running cmd `%s`", cmd)
-		c := exec.Command("sh", "-c", cmd)
-		c.Stdout = os.Stdout
-		c.Stderr = os.Stderr
-		if err := c.Run(); err != nil {
-			return fmt.Errorf("failed to run %s: %v", cmd, err)
+		out, err := DefaultShell.Execute(context.Background(), cmd)
+		if err != nil {
+			return results, fmt.Errorf("failed to run %s: %v", cmd, err)
 		}
+		results = append(results, string(out))
 	}
-	return nil
+	return results, nil
 }
 
 func SetPassword(password string) error {
